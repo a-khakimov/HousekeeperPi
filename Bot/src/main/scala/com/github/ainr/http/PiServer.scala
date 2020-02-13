d9b38e7eecc6de959a939488649d80b23688ec0f
a-khakimov <techlinked@yandex.ru>
2020-02-14 00:12:18 +0500

    can receive images

--- Bot/src/main/scala/com/github/ainr/http/PiServer.scala
+++ Bot/src/main/scala/com/github/ainr/http/PiServer.scala
@@ -1,7 +1,8 @@
 package com.github.ainr.http
 
-import java.io.FileOutputStream
-import java.util.concurrent.SynchronousQueue
+import java.io.{BufferedOutputStream, File, FileOutputStream}
+import java.nio.file.Paths
+import java.util.concurrent.{Future, SynchronousQueue}
 
 import akka.http.scaladsl.model.{HttpEntity, StatusCodes}
 import akka.http.scaladsl.model.ContentTypes._
@@ -12,31 +13,36 @@ import akka.http.scaladsl.server.{PathMatcher, Route}
 import scala.concurrent.duration._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Directive0
+import akka.http.scaladsl.server.directives.FileInfo
+import akka.stream.scaladsl.FileIO
 import spray.json._
+
+import scala.util.{Failure, Success}
 
 object PiServer {
   val server: Route = withoutSizeLimit {
     concat(
       path("png") {
+        // curl -o /dev/null -X POST --header "Content-Type: image/png" --data-binary @img.png http://localhost:8081/png
         post {
           extractStrictEntity(3.second) {
             entity => {
-              //println(entity.contentType)
-              val data = entity.data
-              println(data)
-              //println(entity.data.toArray)
-              val out = Some(new FileOutputStream("/tmp/Test.png"))
-              out.get.write(entity.data.toArray)
+              println(entity.getData())
+              val bos = new BufferedOutputStream(new FileOutputStream("/tmp/i.png"))
+              bos.write(entity.getData().toArray)
+              bos.close()
               complete(StatusCodes.OK)
             }
           }
         }
       },
       path("json") {
+        // curl --header "Content-Type: text/json" -d "{\"k\":\"1\", \"v\":2}" http://localhost:8081/png
         post {
           extractStrictEntity(3.second) {
             entity => {
-              println(entity.contentType)
+              //println(entity.contentType)
+              println(entity.data.utf8String.toString)
               val json = entity.data.utf8String.asJson.asJsObject.fields
               val v = json.getOrElse("v", "No such param")
               println(v)
